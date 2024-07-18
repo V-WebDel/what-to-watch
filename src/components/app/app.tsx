842c6c8a8ecfff470821b7875457deb663a4ab62
V-WebDel <viktor.webdel@gmail.com>
2024-07-18 18:21:43 +0300

    2.10. Добавил страницы и компоненты для страницы Main

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,16 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../../pages/main/main';
+
+type AppProps = {
+  cardsCount: number;
+  dataFilm: {
+    name: string;
+    genre: string;
+    year: number;
+  };
+}
+
+function App({cardsCount, dataFilm}: AppProps): JSX.Element {
+  return <Main cardsCount={cardsCount} name={dataFilm.name} genre={dataFilm.genre} year={dataFilm.year} />;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const CARDS_COUNT = 20;
+
+const DATA_FILM = {
+  name: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  year: 2014,
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App cardsCount={CARDS_COUNT} dataFilm={DATA_FILM} />
   </React.StrictMode>,
 );
